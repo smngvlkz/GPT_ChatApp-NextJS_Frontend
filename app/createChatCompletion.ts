2a102e5789583bb57b93b25ca317d71e292ee58d
TheCodingPantsula <asmdigital14@gmail.com>
2024-05-16 21:35:03 +0200

    Finshed fronted setup of the chatapp

---
+++ app/createChatCompletion.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { ChatCompletionMessage } from "./chat-completion-message.interface";
+
+export default async function createChatCompletion(
+  messages: ChatCompletionMessage[]
+) {
+  const response = await fetch(`${process.env.API_URL}/opnai/chatCompletion`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      messages,
+    }),
+  });
+
+  return response.json();
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,9 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"), require("daisyui")],
+  daisyui: {
+    themes: ["Nord"],
+  },
 };
 export default config;
